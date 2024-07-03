ed6c02e5805e81479ac227b2bfb4a9d843cd7069
wstammSeidor <wstamm@seidoranalytics.com>
2024-07-03 18:36:50 -0300

    test

---
+++ app.py
@@ -0,0 +1,7 @@
+import random
+import time
+
+while True:
+    numero_aleatorio = random.randint(1, 100)  # Genera un número aleatorio entre 1 y 100
+    print(f"Número aleatorio: {numero_aleatorio}")
+    time.sleep(10)  # Pausa la ejecución durante 10 segundos
